d8bfecd53cc43b7579edd6a57e886b411392b2a9
Harpo <harpo@harpo.me>
2024-08-10 13:48:52 -0700

    Try: make output dir

--- render_pdf.sh
+++ render_pdf.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 MUSESCORE=mscore
+OUTPUT_DIR=dist
+
+mkdir -p $OUTPUT_DIR
+
 
 for i in music/*.mscz
 do
     echo "Rendering: $i"
-    "$MUSESCORE" -o "dist/${i%.mscz}.pdf" "$i"
+    "$MUSESCORE" -o "$OUTPUT_DIR/${i%.mscz}.pdf" "$i"
 done
 echo "Finished rendering PDFs"
 ls -al dist/
